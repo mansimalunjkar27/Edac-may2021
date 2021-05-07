936b798b8889f3ad9a36d9e185273d5464dfee94
mansi malunjkar <mansimalunjkar2018@gmail.com>
2021-05-07 19:19:37 +0530

    Add files via upload

---
+++ Assignment1/Assignment2.java
@@ -0,0 +1,23 @@
+/*2.Write a java program to print the sum of two numbers*/
+class Assignment2
+{
+	public static void main(String args[])
+	{
+		
+		
+		int a=74,b=36,c;
+		c=a+b;
+		System.out.println(" first number :"+a);
+		System.out.println(" second number :"+b);
+		System.out.println(" TEST DATA :"+c);
+		
+	}
+}
+/* OUTPUT :
+C:\Users\MANSI\OneDrive\Desktop\cdac\practical_section>javac Assignment2.java
+
+C:\Users\MANSI\OneDrive\Desktop\cdac\practical_section>java Assignment2
+ first number :74
+ second number :36
+ TEST DATA :110
+*/

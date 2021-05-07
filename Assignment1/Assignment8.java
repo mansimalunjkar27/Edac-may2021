7c3b57d1f35e3714b90c388726965d7166271ea1
mansi malunjkar <mansimalunjkar2018@gmail.com>
2021-05-07 19:51:46 +0530

    Update Assignment8.java

--- Assignment1/Assignment8.java
+++ Assignment1/Assignment8.java
@@ -9,13 +9,3 @@ class Assignment8
 		System.out.println(" jj a     a   v   a     a");
 	}
 }
-/*output :
-C:\Users\MANSI\OneDrive\Desktop\cdac\practical_section\Assignment1>javac Assignment8.java
-
-C:\Users\MANSI\OneDrive\Desktop\cdac\practical_section\Assignment1>java Assignment8
-   j   a   v      v  a
-   j  a a   v    v  a a
-j  j aaaaa   v  v  aaaaa
- jj a     a   v   a     a
-
-*/

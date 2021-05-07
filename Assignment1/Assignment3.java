40fe7189d6a5e58d9d136796945a29cebb2f6a55
mansi malunjkar <mansimalunjkar2018@gmail.com>
2021-05-07 19:20:37 +0530

    Add files via upload

---
+++ Assignment1/Assignment3.java
@@ -0,0 +1,21 @@
+/*3. Write a java program ti divide two numbers and print on the screen. */
+class Assignment3
+{
+	public static void main(String args[])
+	{
+		int a=50,b=3,c;
+		c=a/b;
+		System.out.println(" first number :"+a);
+		System.out.println(" second number :"+b);
+		System.out.println(" TEST DATA :"+c);
+		
+	}
+}
+/*OUTPUT: 
+C:\Users\MANSI\OneDrive\Desktop\cdac\practical_section>javac Assignment3.java
+
+C:\Users\MANSI\OneDrive\Desktop\cdac\practical_section>java Assignment3
+ first number :50
+ second number :3
+ TEST DATA :16
+*/

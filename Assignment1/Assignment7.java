c84b36c83cbbde07f933a310cf5fcc3f2bece4a9
mansi malunjkar <mansimalunjkar2018@gmail.com>
2021-05-07 19:51:00 +0530

    Update Assignment7.java

--- Assignment1/Assignment7.java
+++ Assignment1/Assignment7.java
@@ -10,19 +10,3 @@ public class Assignment7
 		}
 	}
 }
-/*OUTPUT:
-
-C:\Users\MANSI\OneDrive\Desktop\cdac\practical_section>javac Assignment7.java
-
-C:\Users\MANSI\OneDrive\Desktop\cdac\practical_section>java Assignment7
-1*8=8
-2*8=16
-3*8=24
-4*8=32
-5*8=40
-6*8=48
-7*8=56
-8*8=64
-9*8=72
-10*8=80
-*/
